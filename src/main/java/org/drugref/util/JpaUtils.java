e52921ea9b9d604b9bec2cdb7c50f75e46ebba7e
jackson <jackson@jackson-laptop>
2010-06-24 11:49:44 -0400

    -remove unused import

--- src/main/java/org/drugref/util/JpaUtils.java
+++ src/main/java/org/drugref/util/JpaUtils.java
@@ -27,7 +27,6 @@ package org.drugref.util;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
 public class JpaUtils {
 
