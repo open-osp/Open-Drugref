d31560fbb5971d92e6bf6640bd4cc27a1d8fb0a9
jackson <jackson@jackson-laptop>
2011-03-30 10:12:21 -0400

    -ID:4 -history table

---
+++ src/main/java/org/drugref/ca/dpd/History.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.drugref.ca.dpd;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author jackson
+ */
+@Entity
+@Table(name = "history")
+@NamedQueries({
+    @NamedQuery(name = "History.findAll", query = "SELECT h FROM History h"),
+    @NamedQuery(name = "History.findById", query = "SELECT h FROM History h WHERE h.id = :id"),
+    @NamedQuery(name = "History.findByDateTime", query = "SELECT h FROM History h WHERE h.dateTime = :dateTime"),
+    @NamedQuery(name = "History.findByAction", query = "SELECT h FROM History h WHERE h.action = :action")})
+public class History implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Basic(optional = false)
+    //@Column(name = "id")
+    private Integer id;
+    @Column(name = "date_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateTime;
+    @Column(name = "action")
+    private String action;
+
+    public History() {
+    }
+
+    public History(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof History)) {
+            return false;
+        }
+        History other = (History) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.drugref.ca.dpd.History[id=" + id + "]";
+    }
+
+}
