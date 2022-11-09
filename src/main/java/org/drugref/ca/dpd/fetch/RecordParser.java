d3ac84f5d0d332385ca246ec31ce50f3b00d47cf
Peter H-C <phc@srpc.ca>
2022-11-09 17:41:02 +0000

    RecordParser.java reversion with note

--- src/main/java/org/drugref/ca/dpd/fetch/RecordParser.java
+++ src/main/java/org/drugref/ca/dpd/fetch/RecordParser.java
@@ -89,7 +89,8 @@ public class RecordParser {
 
     static public Date getDate(String s) throws Exception {
     // drug.txt "10000","","Veterinary","00813761","LINCOMIX","","N","69580","1","03-DEC-2018","0105826008","Vétérinaire","",""
-        DateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+    // dd-MMM-yy format will correctly handle both 03-DEC-2018 and 03-DEC-18
+        DateFormat formatter = new SimpleDateFormat("dd-MMM-yyy");
         Date date = (Date) formatter.parse(s);
         return date;
     }
